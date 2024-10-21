eebca04815b2fbca92b612f35205f9611f9587ee
odinalx <odin.alexandre26@gmail.com>
2024-10-21 13:41:52 +0200

    feat: structure

--- app/src/core/repositoryInterfaces/RepositoryEntityNotFoundException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace toubeelib\core\repositoryInterfaces;
-
-class RepositoryEntityNotFoundException extends \Exception
-{
-
-}
--- app/src/core/services/praticien/ServicePraticienInvalidDataException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace toubeelib\core\services\praticien;
-
-class ServicePraticienInvalidDataException extends \Exception
-{
-
-}

00f6bf0785f9b9fd484cdc3ecc0ad389a7fcff8f
odinalx <odin.alexandre26@gmail.com>
2024-10-22 15:57:12 +0200

    feat: get soiree by id

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use nrv\application\actions\GetSpectacleIdByAction;
+use nrv\application\actions\GetSoireeByIdAction;
 
 return function( \Slim\App $app):\Slim\App {
 
@@ -12,6 +13,8 @@ return function( \Slim\App $app):\Slim\App {
     //Routes Spectacle
     $app->get('/spectacles/{id}', GetSpectacleIdByAction::class);
 
+    //Routes Soiree
+    $app->get('/soirees/{id}', GetSoireeByIdAction::class);
 
     return $app;
 };
---
+++ app/src/application/actions/GetSoireeByIdAction.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace nrv\application\actions;
+
+use nrv\core\services\soiree\ServiceSoireeInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use nrv\application\renderer\JsonRenderer;
+use nrv\core\services\spectacle\ServiceSpectacleNotFoundException;
+use nrv\core\services\soiree\ServiceSoiree;
+
+class GetSoireeByIdAction extends AbstractAction
+{
+    
+    private ServiceSoireeInterface $serviceSoiree;
+
+    public function __construct(ServiceSoiree $serviceSoiree)
+    {
+        $this->serviceSoiree = $serviceSoiree;
+    }
+
+    public function __invoke(Request $rq, Response $rs, array $args): Response
+    {
+        $id = (string) $args['id'];
+
+        try {
+            $soireeDto = $this->serviceSoiree->getSoireeById($id);
+            $responseData = [
+                'self' => "/soirees/{$soireeDto->soiree_id}",
+                'nom' => $soireeDto->nom,
+                'theme' => $soireeDto->theme,
+                'date' => $soireeDto->date->format('d-m-Y'),
+                'horaire_debut' => $soireeDto->horaire_debut,
+                'lieu_id' => $soireeDto->lieu_id,
+                'tarif_normal' => $soireeDto->tarif_normal,
+                'tarif_reduit' => $soireeDto->tarif_reduit,
+            ];
+            return JsonRenderer::render($rs, 200, $responseData);
+
+        } catch (ServiceSpectacleNotFoundException $e) {
+            return JsonRenderer::render($rs, 404, ['error' => $e->getMessage()]);
+        }
+    }
+}
